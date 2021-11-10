7159c0c90ae9ef09b74b17438e0cf57b5958f63f
DoDuc1003 <76798204+DoDuc1003@users.noreply.github.com>
2021-11-10 16:25:32 +0700

    da test va thanh cong toan bo

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package com.company;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -8,15 +8,18 @@ import java.util.*;
 import java.io.*;
 import javax.sound.sampled.*;
 public class Main {
-
-
+    static PhanTu<Integer> pt = new PhanTu<>();
+    static int[] a = new int[100];
     public static void readFromFile(String pathInputFile, PhanTu<Integer> pt) {
         Path path = Path.of(pathInputFile);
         try {
             List<String> list = Files.readAllLines(path);
-            for (String element : list) {
-                pt.insertItem(Integer.valueOf(element));
+            String[] x = list.get(0).split(" ");
+            for (int i = 0; i < x.length; i++) {
+                a[i] = Integer.valueOf(x[i]);
+                pt.insertItem(a[i]);
             }
+            System.out.println("done");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -25,9 +28,8 @@ public class Main {
     public static void writeToFile(String pathOutputFile, PhanTu<Integer> pt) {
         try {
             FileWriter write = new FileWriter(pathOutputFile);
-            Integer[] X = pt.toArr();
-            for (int i : X) {
-                write.write(i + '\n');
+            for (int i = 0; i < pt.size(); i++) {
+                write.write(pt.getValue(i) + " ");
             }
             write.close();
         } catch (IOException e) {
@@ -35,21 +37,24 @@ public class Main {
         }
     }
 
+    public static void insertionSort(PhanTu<Integer> pt) {
+        List<Integer> a = pt.toArr();
+        Collections.sort(a);
+        pt.setAll(a);
+    }
+
     public static void main(String[] args) {
-        PhanTu<Integer> pt = new PhanTu<Integer>() {
-            @Override
-            public boolean greaterThan(Integer a, Integer b) {
-                if (a > b) {
-                    return true;
-                }
-                return false;
-            }
-        };
+
         readFromFile(args[0], pt);
-        pt.insertionSort();
-        writeToFile(args[0], pt);
-
-
+        /*pt.insertItem(3);
+        pt.insertItem(5);
+        pt.insertItem(2);
+        pt.insertItem(1);
+        pt.insertItem(4);*/
+        pt.print();
+        insertionSort(pt);
+        pt.print();
+        writeToFile(args[1], pt);
 
     }
 
--- PhanTu.java
+++ PhanTu.java
@@ -1,56 +1,43 @@
-package com.company;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public abstract class PhanTu <T> {
-    private T[] a;
-    private int length;
+public class PhanTu <T extends Comparable<T>> {
+    public List<T> a;
 
     public PhanTu() {
-        a = (T[]) new Object[1];
-        length = 0;
+        a = new ArrayList<T>();
     }
 
-    private void reSize(int newLength) {
-        T[] newIteam = (T[]) new Object[newLength];
-        for (int i = 0; i < length; i++) {
-            newIteam[i] = a[i];
-        }
-        a = newIteam;
+    public void setAll(List<T> t) {
+        a = t;
     }
 
     public void insertItem(T add) {
-        if (length == a.length) {
-            reSize(a.length * 2);
-        }
-        a[length++] = add;
+        a.add(add);
+        System.out.println("done");
     }
 
-    public abstract boolean greaterThan(T a, T b);
-
-    public void insertionSort() {
-        for (int i = 0; i < length - 1; i++) {
-            for (int j = i; j > 0; j--){
-                if (greaterThan(a[j - 1], a[j])) {
-                    T temp = a[j-1];
-                    a[j-1] = a[j];
-                    a[j] = temp;
-                } else {
-                    break;
-                }
-            }
+    public void print() {
+        for (T t : a) {
+            System.out.print(t + " ");
         }
+        System.out.println();
     }
 
-    public T[] toArr() {
-        T[] res = (T[]) new Object[a.length];
-        for (int i = 0; i < a.length; i++) {
-            res[i] = a[i];
-        }
-        return res;
+    public T getValue(int index) {
+        return a.get(index);
+    }
+
+
+
+    public int size() {
+        return a.size();
+    }
+
+    public List<T> toArr() {
+        return a;
     }
 }
 
